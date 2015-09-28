c7e31fc7a534aa65c1495ee8028913328f27a08f
Jason Hejna <jason.hejna@gmail.com>
2015-09-27 23:34:59 -0400

    some info to the README

--- python.py
+++ python.py
@@ -1,16 +1,7 @@
-#!/usr/bin/python3
+def solveMeFirst(a,b):
+  return a+b
 
-inputString = raw_input();
-print inputString
-inputString = raw_input();
-print inputString
-
-def solveMeSecond(a,b):
-   return a+b
-
-#n = int(input())
-#for i in range(0,n):
-#    a, b = input().split()
-#    a, b = int(a),int(b)
-#    res = solveMeSecond(a,b)
-#    print (res)
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
