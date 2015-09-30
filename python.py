335bc70fb4daec265bc4562c8a8d3870efc9916e
Jason Hejna <jason.hejna@gmail.com>
2015-09-29 20:17:39 -0400

    rollin python

--- python.py
+++ python.py
@@ -2,7 +2,14 @@
 import sys
 
 n = int(sys.stdin.readline())
-inputArray = str(sys.stdin.readline())
 
+staircase = ''
+for i in range(1,n+1):
+	stair = ''
+	for j in range(0,n-i):
+		stair += ' '
+	for k in range(0,i):
+		stair += '#'
+	staircase += stair + '\n'
 
-sys.stdout.write(inputArray)
+sys.stdout.write(staircase)
