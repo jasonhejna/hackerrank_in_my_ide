3bcbce1bd8480a3e74793b317b7e0fdd7b931713
Jason Hejna <jason.hejna@gmail.com>
2015-09-29 20:38:27 -0400

    python on fleak

--- python.py
+++ python.py
@@ -1,15 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""This script is for the staircase hackerrank code test"""
+
 import sys
 
-n = int(sys.stdin.readline())
+INPUT_LEN = int(sys.stdin.readline())
 
-staircase = ''
-for i in range(1,n+1):
-	stair = ''
-	for j in range(0,n-i):
-		stair += ' '
-	for k in range(0,i):
-		stair += '#'
-	staircase += stair + '\n'
+STAIR_CASE = ''
+for i in range(1, INPUT_LEN+1):
+    stair = ''
+    for j in range(0, INPUT_LEN-i):
+        stair += ' '
+    for k in range(0, i):
+        stair += '#'
+    STAIR_CASE += stair + '\n'
 
-sys.stdout.write(staircase)
+sys.stdout.write(STAIR_CASE)
